d2309a3bd6b2e2758e765ce796339e0980e1d7aa
yuc0805 <yuc204@ucsd.edu>
2025-04-22 21:19:23 +0000

    dataset updated

--- MoCA/util/datasets.py
+++ MoCA/util/datasets.py
@@ -90,7 +90,7 @@ class iWatch(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return 20 #len(os.listdir(self.data_path))
+        return len(os.listdir(self.data_path))
 
     def __getitem__(self, idx):
         # load the data
