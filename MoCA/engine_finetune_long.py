3578a97b32a4777b4b46af25c6f6679bef451604
yuc0805 <yuc204@ucsd.edu>
2025-04-30 02:14:09 +0000

    init attn probe

---
+++ MoCA/engine_finetune_long.py
@@ -0,0 +1,181 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+
+import math
+import sys
+from typing import Iterable, Optional
+
+import torch
+
+from timm.data import Mixup
+from timm.utils import accuracy
+from torchmetrics.classification import MulticlassRecall, MulticlassSpecificity,MulticlassF1Score
+
+import util.misc as misc
+import util.lr_sched as lr_sched
+
+
+def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    epoch: int, loss_scaler, max_norm: float = 1,
+                    mixup_fn: Optional[Mixup] = None, log_writer=None, device = torch.device,
+                    args=None):
+    model.train(True)
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
+
+    accum_iter = args.accum_iter
+
+    optimizer.zero_grad()
+
+    if log_writer is not None:
+        print('log_dir: {}'.format(log_writer.dir))
+
+    
+    for data_iter_step, (samples, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+        # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % accum_iter == 0:
+            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+        samples = samples.to(device, non_blocking=True) # BS, 42, 100, 3
+        targets = targets.to(device, non_blocking=True) # BS,42
+
+        with torch.cuda.amp.autocast(enabled=False):
+            outputs = model(samples) # BS,42,2
+
+            outputs = outputs.view(-1, outputs.size(-1))  # (BS * 42, 2)
+            targets = targets.view(-1)  
+               
+            loss = criterion(outputs, targets)
+
+        loss_value = loss.item()
+
+        acc1, _ = accuracy(outputs, targets, topk=(1, 3))
+        batch_size = samples.shape[0]
+    
+        metric_logger.meters['train_acc1'].update(acc1.item(), n=batch_size)
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        loss /= accum_iter
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                    parameters=model.parameters(), create_graph=False,
+                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        if (data_iter_step + 1) % accum_iter == 0:
+            optimizer.zero_grad()
+
+        torch.cuda.synchronize() 
+
+        metric_logger.update(loss=loss_value)
+
+        min_lr = 10.
+        max_lr = 0.
+        for group in optimizer.param_groups:
+            min_lr = min(min_lr, group["lr"])
+            max_lr = max(max_lr, group["lr"])
+
+        metric_logger.update(lr=max_lr)
+
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+
+
+
+        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+            """ We use epoch_1000x as the x-axis in tensorboard.
+            This calibrates different curves when batch size changes.
+            """
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            log_writer.log(
+                {'loss': loss_value_reduce, 
+                 'lr': max_lr, 
+                 'train acc1': acc1,
+                #  'train bal_acc':bal_acc,
+                #  'train f1':f1,
+                 }, step=epoch_1000x)
+
+
+            # log_writer.add_scalar('loss', loss_value_reduce, epoch_1000x)
+            # log_writer.add_scalar('lr', max_lr, epoch_1000x)
+            # log_writer.add_scalar('train acc1', acc1, epoch_1000x)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+@torch.no_grad()
+def evaluate(args,data_loader, model, device):
+    criterion = torch.nn.CrossEntropyLoss()
+
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+
+    recall_metric = MulticlassRecall(args.nb_classes,
+                            average='weighted',
+                            zero_division=0).to(device)
+    
+    specificity_metric  = MulticlassSpecificity(num_classes=2,
+                            average='weighted',
+                            zero_division=0).to(device)
+    f1_metric = MulticlassF1Score(num_classes=args.nb_classes,
+                            average='weighted',
+                            zero_division=0).to(device)
+    
+    for batch in metric_logger.log_every(data_loader, 10, header):
+        images = batch[0]
+        target = batch[-1]
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+
+        # compute output
+        # with torch.cuda.amp.autocast():
+        
+        #output = model(images,mask_ratio=0)
+        output = model(images)
+        loss = criterion(output, target)
+
+        acc1, _ = accuracy(output, target, topk=(1, 2))     
+
+        preds = output.argmax(dim=1)
+        recall_metric.update(preds, target)
+        specificity_metric.update(preds, target)
+        f1_metric.update(preds, target)
+
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    # Compute metrics
+    recall_tm = recall_metric.compute().item()
+    specificity_tm = specificity_metric.compute().item()
+    bal_acc = 100 * (recall_tm + specificity_tm) / 2
+    f1 = 100 * f1_metric.compute().item()
+
+    print('* Acc@1 {top1.global_avg:.5f} bal_acc {bal_acc:.5f} f1 {f1:.5f} loss {losses.global_avg:.3f}'  
+          .format(top1=metric_logger.acc1, bal_acc=bal_acc, f1=f1, losses=metric_logger.loss))
+
+    eval_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    eval_stats['f1']=f1
+    eval_stats['bal_acc']=bal_acc
+
+    return eval_stats
---
+++ MoCA/main_linprobe_long.py
@@ -0,0 +1,413 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# MoCo v3: https://github.com/facebookresearch/moco-v3
+# --------------------------------------------------------
+
+import argparse
+import datetime
+import json
+import os
+import time
+from pathlib import Path
+import pickle
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import wandb
+
+import timm
+from config import LP_DATASET_CONFIG
+from util.datasets import iWatch_HDf5, data_aug,collate_fn,resample_aug
+import util.misc as misc
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+from timm.optim import create_optimizer_v2
+from util.pos_embed import interpolate_pos_embed
+import util.lr_decay as lrd  # for optimizer
+import models_vit
+
+from engine_finetune_long import train_one_epoch, evaluate
+import sys
+sys.path.append('/DeepPostures_MAE/MSSE-2021-pt')
+from commons import get_dataloaders_dist
+import random
+from eniops import arrange
+
+# helper function
+def parse_list(input_string):
+    # Strip brackets and split the string into a list of integers
+    return [int(x) for x in input_string.strip('[]').split(',')]
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE linear probing for image classification', add_help=False)
+    parser.add_argument('--batch_size', default=128, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=20, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+
+    # Model parameters
+    parser.add_argument('--model', default='vit_base_patch16', type=str, metavar='MODEL',
+                        help='Name of model to train')
+    parser.add_argument('--input_size', type=int, default=100, 
+                        help='Input size "')
+    parser.add_argument('--patch_size', type=int, default=5, 
+                        help='Patch size')
+
+    parser.add_argument('--in_chans', default=3, type=int,  # changed - added
+                        help='number of channels')
+    parser.add_argument('--remark', default='Debug',type=str,
+                        help='model_remark')
+
+    # Optimizer parameters
+    parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM',
+                        help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--weight_decay', type=float, default=0,
+                        help='weight decay (default: 0 for linear probe following MoCo v1)')
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-2, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=2, metavar='N',
+                        help='epochs to warmup LR')
+    
+
+    # * Finetuning params
+    parser.add_argument('--checkpoint', default='/home/jovyan/persistent-data/MAE_Accelerometer/experiments/661169(p200_10_alt_0.0005)/checkpoint-3999.pth', 
+                        type=str,help='model checkpoint for evaluation') 
+                        
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='/niddk-data-central/iWatch/pre_processed_seg/W', type=str, # changed
+                        help='dataset path')
+    
+    parser.add_argument('--nb_classes', default=7, type=int, # changed
+                        help='number of the classification types')
+
+    parser.add_argument('--output_dir', default='/niddk-data-central/leo_workspace/MoCA_result/LP/ckpt',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='/niddk-data-central/leo_workspace/MoCA_result/LP/log',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--eval', default=None, type=str,
+                        help='Perform evaluation only')
+    parser.add_argument('--dist_eval', action='store_true', default=False,
+                        help='Enabling distributed evaluation (recommended during training for faster monitor')
+    parser.add_argument('--num_workers', default=2, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+    parser.add_argument('--ds_name', default='iwatch', type=str)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser    
+
+class AttentionProbeModel(nn.Module):
+    def __init__(self, base_model, window_size=42,num_classes=2,hidden_dim=256):
+        super(AttentionProbeModel, self).__init__()
+        self.base_model = base_model
+        self.base_model.head = nn.Identity()  # Remove the original head
+        self.window_size = window_size
+        self.proj = nn.Linear(self.base_model.embed_dim, hidden_dim)
+        self.attn = nn.TransformerEncoderLayer(
+            d_model=hidden_dim,
+            nhead=8,
+            dim_feedforward=hidden_dim*2,
+            batch_first=True
+        )
+        self.head = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, x):
+        '''
+        input: x [BS, 42, 100, 3]
+        '''
+        
+        # get feature for each window
+        x = arrange(x, 'b,w,l,c -> (b w) c l') # BS*42, 3,100
+        x = x.unsqueeze(1)  # BS*42, 1, 3, 100
+        with torch.no_grad():
+            x = self.base_model(x) # BS*42, 768
+
+        x = arrange(x, '(b w) c -> b w c', b=x.shape[0]//self.window_size, w=self.window_size) # BS, 42, 768
+        x = self.proj(x) # BS, 42, 256
+        x = self.attn(x) # BS, 42, 256
+        x = self.head(x) # BS, 42, num_classes
+
+        return x
+
+def main(args):
+    
+
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+    
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    cudnn.benchmark = False 
+    
+    if args.ds_name == 'iwatch':
+        with open("/niddk-data-central/iWatch/support_files/iwatch_split_dict.pkl", "rb") as f:
+            split_data = pickle.load(f)
+
+        train_subjects = split_data["train"]
+        valid_subjects = split_data["val"]
+        
+
+        random.shuffle(train_subjects)
+        random.shuffle(valid_subjects)
+
+        data_loader_train, data_loader_val, _ = get_dataloaders_dist(
+        pre_processed_dir=args.data_path,
+        bi_lstm_win_size=42, # chap_adult
+        batch_size=args.batch_size,
+        train_subjects=train_subjects,
+        valid_subjects=valid_subjects,
+        test_subjects=None,)
+
+
+        
+    else:
+        raise NotImplementedError('The specified dataset is not implemented.')
+
+
+
+    if True:  # args.distributed:
+        num_tasks = misc.get_world_size()
+        global_rank = misc.get_rank()
+
+    if args.log_dir is not None and not args.eval and global_rank == 0:  
+        wandb.login(key='32b6f9d5c415964d38bfbe33c6d5c407f7c19743')
+        log_writer = wandb.init(
+            project='MoCA-iWatch-attention-probe',  # Specify your project
+            config= vars(args),
+            dir=args.log_dir,
+            name=args.remark,)
+      
+    else:
+        log_writer = None
+
+    base_model = models_vit.__dict__[args.model](
+            img_size=args.input_size, patch_size=[1, int(args.patch_size)], 
+            num_classes=args.nb_classes, in_chans=1, 
+            global_pool=False)
+    
+
+
+    # # load weight
+    if not args.eval:
+        print('Loading pre-trained checkpoint from',args.checkpoint)
+        checkpoint = torch.load(args.checkpoint,map_location='cpu')
+        checkpoint_model = checkpoint['model']
+        interpolate_pos_embed(base_model, checkpoint_model,orig_size=(3,20), # FIXME: can also be [6,20] if using both HIP and Wrist
+                              new_size=(args.input_size[0],int(args.input_size[1]//args.patch_size)))
+        # interpolate_pos_embed(model, checkpoint_model,orig_size=(6,10), # FIXME: can also be [6,20] if using both HIP and Wrist
+        #                       new_size=(args.input_size[0],int(args.input_size[1]//args.patch_size)))
+
+        #print(checkpoint_model.keys())
+        decoder_keys = [k for k in checkpoint_model.keys() if 'decoder' in k]
+        for key in decoder_keys:
+            del checkpoint_model[key]
+
+        print('shape after interpolate:',checkpoint_model['pos_embed'].shape)
+        msg = base_model.load_state_dict(checkpoint_model, strict=False)
+        print(msg)
+    # else:
+    #     checkpoint = torch.load(args.eval,map_location='cpu')
+    #     checkpoint_model = checkpoint['model']
+    #     print(checkpoint['args'])
+    #     msg = model.load_state_dict(checkpoint_model, strict=True)
+    #     model.to(device)
+    #     test_stats = evaluate(args,data_loader_val, model, device)
+    #     print(f"Balanced Accuracy of the network on the test images: {test_stats['bal_acc']:.5f}% and F1 score of {test_stats['f1']:.5f}%")
+    #     exit(0)
+
+    #freeze weight
+    base_model.head = nn.Identity()
+    for _, p in base_model.named_parameters():
+        p.requires_grad = False
+
+    model = AttentionProbeModel(base_model, window_size=42,num_classes=args.nb_classes,hidden_dim=256)
+    print("Model = %s" % str(model))
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('number of training params : %.2f' % (n_parameters))
+
+    n_total_parameters = sum(p.numel() for p in model.parameters())
+    print('Total number of parameters: %.2f' % (n_total_parameters))
+
+    model.to(device)
+    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
+    model_without_ddp = model
+    
+    if args.lr is None:  # only base_lr is specified
+        args.lr = args.blr * eff_batch_size / 256
+
+    print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
+    print("actual lr: %.2e" % args.lr)
+
+    print("accumulate grad iterations: %d" % args.accum_iter)
+    print("effective batch size: %d" % eff_batch_size)
+    
+    print("lr: %.3e" % args.lr)
+
+    if args.distributed: #changed - hashed out
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+
+    optimizer = create_optimizer_v2(
+        model_without_ddp,
+        opt='adamw',
+        lr=args.lr,
+        weight_decay=args.weight_decay,# default: 0 
+        betas=(0.9, 0.95))
+
+    loss_scaler = NativeScaler()
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    print("criterion = %s" % str(criterion))
+
+    print(f"Start training for {args.epochs} epochs")
+    start_time = time.time()
+    max_accuracy = 0.0
+    best_metric = {'epoch':0, 'acc1':0.0, 'bal_acc':0.0, 'f1':0.0}
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed: 
+            data_loader_train.sampler.set_epoch(epoch)
+        train_stats = train_one_epoch(
+            model, criterion, data_loader_train,
+            optimizer, epoch, loss_scaler,
+            max_norm=args.clip_grad, 
+            log_writer=log_writer,
+            args=args, device=device,
+        )
+        if args.output_dir and (epoch + 1 == args.epochs):
+            misc.save_model(
+                args=args, model=model, model_without_ddp=model, optimizer=optimizer,
+                loss_scaler=loss_scaler, epoch=epoch)
+
+        test_stats = evaluate(args,data_loader_val, model, device)
+        print(f"Balanced Accuracy of the network on the test images: {test_stats['bal_acc']:.5f} and F1 score of {test_stats['f1']:.5f}%")
+        # save the best epoch
+        if max_accuracy < test_stats["bal_acc"]:
+            max_accuracy = test_stats["bal_acc"]
+
+            best_metric['epoch'] = epoch
+            best_metric['bal_acc'] = test_stats['bal_acc']
+            best_metric['acc1'] = test_stats['acc1']
+            best_metric['f1'] = test_stats['f1']
+
+            if args.output_dir:
+                misc.save_model(
+                    args=args, model=model, model_without_ddp=model, optimizer=optimizer,
+                    loss_scaler=loss_scaler, epoch="best")
+
+        print(f'Max Balanced accuracy: {max_accuracy:.2f}%')
+
+        if log_writer is not None:
+            log_writer.log({'perf/test_acc1': test_stats['acc1'], 
+                            'perf/bal_acc': test_stats['bal_acc'],
+                            'perf/f1': test_stats['f1'],
+                            'perf/test_loss': test_stats['loss'], 
+                            'epoch': epoch})
+
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                     **{f'test_{k}': v for k, v in test_stats.items()},
+                     'epoch': epoch,
+                     'n_parameters': n_parameters}
+
+        if args.output_dir and misc.is_main_process():
+            with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
+                f.write(json.dumps(log_stats) + "\n")
+    if log_writer is not None:
+        log_writer.log({f"best_epoch_{k}": v for k, v in best_metric.items()})
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+    return max_accuracy
+
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+
+    initial_timestamp = datetime.datetime.now()
+    
+
+    args.in_chans = LP_DATASET_CONFIG[args.ds_name]['in_chans']
+    args.nb_classes = LP_DATASET_CONFIG[args.ds_name]['nb_classes']
+    args.blr = LP_DATASET_CONFIG[args.ds_name]["blr"]
+    args.batch_size = LP_DATASET_CONFIG[args.ds_name]["bs"]
+    args.input_size = LP_DATASET_CONFIG[args.ds_name]["input_size"]
+    args.remark = args.remark + f'LP_blr_{args.blr}_bs_{args.batch_size}_input_size_{args.input_size}'
+    print(f'Start Training: {args.remark}')
+    
+    args.log_dir = os.path.join(args.log_dir,args.remark,f'{initial_timestamp.strftime("%Y-%m-%d_%H-%M")}')
+    args.output_dir = os.path.join(args.output_dir,args.remark,f'{initial_timestamp.strftime("%Y-%m-%d_%H-%M")}')
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+        Path(args.log_dir).mkdir(parents=True, exist_ok=True)
+
+    main(args)
+
+
+
+'''
+
+torchrun --nproc_per_node=4  -m main_linprobe \
+--ds_name iwatch \
+--checkpoint "/niddk-data-central/leo_workspace/MoCA_result/ckpt/iWatch-Hipps_5_mask_0.75_bs_256_blr_None_epoch_100/2025-04-23_20-41/checkpoint-20.pth" \
+--data_path "/niddk-data-central/iWatch/pre_processed_seg/H" \
+--remark Hip_20epoch
+
+
+torchrun --nproc_per_node=4  -m main_linprobe \
+--ds_name iwatch \
+--checkpoint "/niddk-data-central/leo_workspace/MoCA_result/ckpt/iWatch-Wristps_5_mask_0.75_bs_256_blr_None_epoch_100/2025-04-25_04-07/checkpoint-20.pth" \
+--data_path "/niddk-data-central/iWatch/pre_processed_seg/W" \
+--remark Wrist_20epoch 
+
+torchrun --nproc_per_node=4  -m main_linprobe \
+--ds_name iwatch \
+--checkpoint "/niddk-data-central/leo_workspace/MoCA_200.pth" \
+--data_path "/niddk-data-central/iWatch/pre_processed_seg/W" \
+--remark Wrist_MoCA200 \
+--patch_size 5 
+
+
+
+'''
