75466bb6ee767ffe232befd24219960043d113e1
yuc0805 <yuc204@ucsd.edu>
2025-05-14 12:51:24 +0000

    hp tunning

--- MoCA/config.py
+++ MoCA/config.py
@@ -12,5 +12,5 @@ FT_DATASET_CONFIG = {
     }
 
 LP_LONG_DATASET_CONFIG = {
-    "iwatch": {"in_chans": 3, "nb_classes": 2, "blr": 1e-4,'bs':4,'input_size':[3,4200],'weight_decay':1e-4}, 
+    "iwatch": {"in_chans": 3, "nb_classes": 2, "blr": 1e-2,'bs':4,'input_size':[3,4200],'weight_decay':5e-2}, 
 }
