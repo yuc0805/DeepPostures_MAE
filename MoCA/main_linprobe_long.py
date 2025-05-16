1f73673e03b82b104f321c30e1f183f492cdf7c1
yuc0805 <yuc204@ucsd.edu>
2025-05-16 21:43:13 +0000

    path

--- MoCA/main_linprobe_long.py
+++ MoCA/main_linprobe_long.py
@@ -39,7 +39,10 @@ from engine_finetune_long import train_one_epoch, evaluate
 
 import pickle
 import sys
-sys.path.append('/app/DeepPostures_MAE/MSSE-2021-pt')
+if os.path.exists('/DeepPostures_MAE/MSSE-2021-pt'):
+    sys.path.append('/DeepPostures_MAE/MSSE-2021-pt')
+elif os.path.exists('/app/DeepPostures_MAE/MSSE-2021-pt'):
+    sys.path.append('/app/DeepPostures_MAE/MSSE-2021-pt')
 from commons import get_dataloaders_dist,data_aug
 import random
 from einops import rearrange
