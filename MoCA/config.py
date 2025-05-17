d34a138ad7a35e01a583fb02dfb3fb35f97c0783
yuc0805 <yuc204@ucsd.edu>
2025-05-17 09:31:07 -0400

    config

--- MoCA/config.py
+++ MoCA/config.py
@@ -12,11 +12,11 @@ FT_DATASET_CONFIG = {
     }
 
 LP_LONG_DATASET_CONFIG = {
-    "iwatch": {"in_chans": 3, "nb_classes": 2, "blr": 1e-2,'bs':8,'input_size':[3,4200],'weight_decay':5e-2}, 
+    "iwatch": {"in_chans": 3, "nb_classes": 2, "blr": 1e-3,'bs':8,'input_size':[3,4200],'weight_decay':5e-2}, 
 }
 
 FT_LONG_DATASET_CONFIG = {
-    "iwatch": {"in_chans": 3, "nb_classes": 2, "blr": 1e-2,'bs':8,'input_size':[3,4200],'weight_decay':1e-4}, 
+    "iwatch": {"in_chans": 3, "nb_classes": 2, "blr": 1e-3,'bs':4,'input_size':[3,4200],'weight_decay':1e-4}, 
 }
 
 
