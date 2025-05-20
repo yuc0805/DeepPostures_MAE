156d4c9bd5d4c44d90e4f5173c59cebf2692a1d0
yuc0805 <yuc204@ucsd.edu>
2025-05-20 15:59:27 +0000

    sync

--- MoCA/engine_finetune_long.py
+++ MoCA/engine_finetune_long.py
@@ -49,7 +49,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         samples = samples.float().to(device, non_blocking=True) # BS, 42, 100, 3
         targets = targets.to(device, non_blocking=True) # BS,42
         batch_size = targets.shape[0] 
-        targets = targets.contiguous().view(-1).squeeze() #(BS*42,)
+        targets = targets.view(-1).squeeze() #(BS*42,)
         if criterion.__class__.__name__ == 'BCEWithLogitsLoss':
             targets = targets.float()
         
@@ -59,7 +59,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         with torch.cuda.amp.autocast(enabled=False):
             outputs = model(samples)
             if args.CHAP:
-                outputs = outputs.contiguous().view(-1)
+                outputs = outputs.view(-1)
             else:
                 outputs = rearrange(outputs, 'b w c -> (b w) c').squeeze()
 
@@ -153,7 +153,7 @@ def evaluate(args,data_loader, model, device):
 
         if criterion.__class__.__name__ == 'BCEWithLogitsLoss':
             target = target.float()
-            target = target.contiguous().view(-1).squeeze()
+            target = target.view(-1).squeeze()
 
         batch_size = target.shape[0]
 
@@ -163,7 +163,7 @@ def evaluate(args,data_loader, model, device):
         with torch.cuda.amp.autocast(enabled=False):
             outputs = model(samples)
             if args.CHAP:
-                outputs = outputs.contiguous().view(-1)
+                outputs = outputs.view(-1)
             else:
                 outputs = rearrange(outputs, 'b w c -> (b w) c').squeeze()
 
@@ -243,8 +243,8 @@ def evaluate_cm(data_loader, model, device, as_img=False, save_dir='./'):
 
         target = target.to(device, non_blocking=True)
         output = model(samples)
-        output = output.contiguous().view(-1, output.size(-1))  # shape: (B*42, C)
-        target = target.contiguous().view(-1)
+        output = output.view(-1, output.size(-1))  # shape: (B*42, C)
+        target = target.view(-1)
 
         loss = criterion(output, target)
 
--- MoCA/models_mae.py
+++ MoCA/models_mae.py
@@ -518,7 +518,7 @@ class LinearProbeModel(nn.Module):
         x = rearrange(x, 'b (c w) d -> b w c d',c=c) # BS, 42, nvar,768
         x = x.mean(dim=2) # BS, 42, 768
         
-        x = self.head(x) # BS, 42, 
+        x = self.head(x) # BS, 42, 1
 
         return x
     
