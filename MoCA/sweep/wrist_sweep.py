d7c143798a1cf3404373e58ab19fc005307c4090
yuc0805 <yuc204@ucsd.edu>
2025-06-05 13:12:57 +0000

    new sweep

--- MoCA/sweep/wrist_sweep.py
+++ MoCA/sweep/wrist_sweep.py
@@ -12,13 +12,14 @@ def main():
         "--nproc_per_node=4",
         "main_finetune_long.py",
         "--ds_name", "iwatch",
-        "--data_path", "/niddk-data-central/iWatch/pre_processed_pt/W",
+        "--data_path", "/niddk-data-central/iWatch/pre_processed_long_seg/W",
         "--remark", f"Wrist_sweep_blr_{config.blr}_wd_{config.weight_decay}",
         "--blr", str(config.blr),
         "--weight_decay", str(config.weight_decay),
-        "--pos_weight", "2.8232",
-        "--batch_size", "16",
+        "--pos_weight", str(config.pos_weight),
+        "--batch_size", str(config.batch_size),
         "--epochs", "40",
+        "--warmup_epochs", str(config.warmup_epochs),
         "--model","CNNBiLSTMModel",
         # Add other necessary arguments here
     ]
