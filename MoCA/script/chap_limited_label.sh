cc723c207396956e8035a81e170962aacf754b81
yuc0805 <yuc204@ucsd.edu>
2025-06-10 02:39:09 +0000

    update bash

---
+++ MoCA/script/chap_limited_label.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Print the current directory
+echo "Current directory: $(pwd)"
+
+# List all files in the current directory
+echo "Files:"
+ls -lah
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
+--remark CHAP  \
+--blr 1e-3 \
+--model CNNBiLSTMModel \
+--epochs 50 \
+--warmup_epochs 5 \
+--batch_size 256 \
+--weight_decay 5e-2 \
+--subset_ratio 0.1 
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
+--remark CHAP  \
+--blr 1e-3 \
+--model CNNBiLSTMModel \
+--epochs 50 \
+--warmup_epochs 5 \
+--batch_size 256 \
+--weight_decay 5e-2 \
+--subset_ratio 0.01 
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
+--remark CHAP  \
+--blr 1e-3 \
+--model CNNBiLSTMModel \
+--epochs 50 \
+--warmup_epochs 5 \
+--batch_size 256 \
+--weight_decay 5e-2 \
+--subset_ratio 0.5
+
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--remark CHAP  \
+--blr 1e-3 \
+--model CNNBiLSTMModel \
+--epochs 50 \
+--warmup_epochs 5 \
+--batch_size 256 \
+--weight_decay 5e-2 \
+--subset_ratio 0.1 
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--remark CHAP  \
+--blr 1e-3 \
+--model CNNBiLSTMModel \
+--epochs 50 \
+--warmup_epochs 5 \
+--batch_size 256 \
+--weight_decay 5e-2 \
+--subset_ratio 0.01 
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--remark CHAP  \
+--blr 1e-3 \
+--model CNNBiLSTMModel \
+--epochs 50 \
+--warmup_epochs 5 \
+--batch_size 256 \
+--weight_decay 5e-2 \
+--subset_ratio 0.5
+
+echo "All tasks completed."
+
+##
+# chmod +x script/chap_limited_label.sh
+# ./script/chap_limited_label.sh
