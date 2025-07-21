cf500bd4166f82428f5d3cc5cfcca19d1b3c4338
yuc0805 <yuc204@ucsd.edu>
2025-07-21 10:30:47 -0400

    sync

---
+++ MoCA/script/moca_shallow_sol.sh
@@ -0,0 +1,15 @@
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/SOL/PASOS/train/SOL_10hz" \
+--remark moca-shallow-random-init  \
+--blr 1e-3 \
+--model shallow-moca \
+--epochs 10 \
+--warmup_epochs 2 \
+--batch_size 8 \
+--accum_iter 2 \
+--weight_decay 5e-2 \
+--subset_ratio 1.0 \
+--pos_weight 1.0 \
+--use_data_aug 1 \
+--drop_path_rate 0.2
