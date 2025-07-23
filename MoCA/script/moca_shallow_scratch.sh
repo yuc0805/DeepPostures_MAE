a32fb5671c9b435cc2f59054946650432995e597
yuc0805 <yuc204@ucsd.edu>
2025-07-23 10:59:51 -0400

    hp tuning

--- MoCA/script/moca_shallow_scratch.sh
+++ MoCA/script/moca_shallow_scratch.sh
@@ -3,10 +3,10 @@ torchrun --nproc_per_node=4 -m main_finetune_long \
 --data_path "/niddk-data-central/SOL/PASOS/train/SOL_10hz" \
 --remark shallow-moca-scratch  \
 --model shallow-moca \
---blr 1e-3 \
---weight_decay 1e-3 \
---epochs 10 \
---warmup_epochs 2 \
+--blr 1e-2 \
+--weight_decay 5e-2 \
+--epochs 40 \
+--warmup_epochs 4 \
 --batch_size 8 \
 --accum_iter 2 \
 --num_attn_layer 2 \
