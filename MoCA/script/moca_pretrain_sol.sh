9160ec37eb62a9859f3ff917e5d66f12d58512a3
yuc0805 <yuc204@ucsd.edu>
2025-07-24 10:37:18 -0400

    sync

--- MoCA/script/moca_pretrain_sol.sh
+++ MoCA/script/moca_pretrain_sol.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node=8 main_pretrain.py \
 --data_path /niddk-data-central/SOL/PASOS/train/SOL_10hz \
---batch_size 16 \
+--batch_size 12 \
 --world_size 8 \
 --epochs 100 \
 --warmup_epochs 10 \
