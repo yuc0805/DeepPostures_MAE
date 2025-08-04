5bb71831f4a60097d4320d60373cb71cccd0069f
yuc0805 <yuc204@ucsd.edu>
2025-08-04 12:02:47 -0400

    bash

--- MoCA/script/moca_pretrain_sol2.sh
+++ MoCA/script/moca_pretrain_sol2.sh
@@ -7,4 +7,5 @@ torchrun --nproc_per_node=8 main_pretrain.py \
 --std_sampling \
 --remark SOL \
 --save_freq 2 \
---mask_ratio 0.5 
+--mask_ratio 0.5 \
+--resume "/niddk-data-central/leo_workspace/MoCA_result/ckpt/SOLps_5_mask_0.5_bs_12_blr_None_epoch_100/2025-07-25_20-16/ckpt.pth"
