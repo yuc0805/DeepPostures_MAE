dcb6680d3ac8113a6b3aa10898c9fdb2858044c0
yuc0805 <yuc204@ucsd.edu>
2025-08-08 10:33:51 -0700

    bash

---
+++ MoCA/script/moca_eval.sh
@@ -0,0 +1,15 @@
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/SOL/PASOS/train/SOL_10hz" \
+--remark moca-shallow-mask50 \
+--checkpoint "/niddk-data-central/leo_workspace/MoCA_result/ckpt/SOLps_5_mask_0.5_bs_12_blr_None_epoch_100/2025-07-25_20-16/checkpoint-14.pth" \
+--blr 1e-3 \
+--model shallow-moca \
+--epochs 20 \
+--warmup_epochs 2 \
+--batch_size 8 \
+--accum_iter 2 \
+--weight_decay 5e-2 \
+--subset_ratio 1.0 \
+--pos_weight 1.0 \
+--use_data_aug 1 
