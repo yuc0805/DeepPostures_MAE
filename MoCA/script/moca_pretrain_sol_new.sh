c2a8bd84539b656b12b997d10898fec46ff83cf0
yuc0805 <yuc204@ucsd.edu>
2025-08-21 21:55:17 +0000

    bash

--- MoCA/script/moca_pretrain_sol_new.sh
+++ MoCA/script/moca_pretrain_sol_new.sh
@@ -1,9 +1,9 @@
 pip install transformers
 
-torchrun --nproc_per_node=2 main_pretrain.py \
+torchrun --nproc_per_node=8 main_pretrain.py \
 --data_path /niddk-data-central/SOL/PASOS/train/SOL_10hz \
 --batch_size 12 \
---world_size 2 \
+--world_size 8 \
 --epochs 100 \
 --warmup_epochs 10 \
 --std_sampling \
