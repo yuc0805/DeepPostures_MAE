4c7ffde8ac77195fbf6b0aeea54d43b852e5ceb3
yuc0805 <yuc204@ucsd.edu>
2025-09-03 20:55:55 +0000

    sync

--- MoCA/script/iwatch_vit_long.sh
+++ MoCA/script/iwatch_vit_long.sh
@@ -9,7 +9,7 @@ torchrun --nproc_per_node=4 -m main_finetune_long \
 --model vit-long \
 --epochs 200 \
 --warmup_epochs 20 \
---batch_size 128 \
+--batch_size 64 \
 --subset_ratio 1.0 \
 --pos_weight 2.7953 \
 --input_size 4200 \
@@ -27,7 +27,7 @@ torchrun --nproc_per_node=4 -m main_finetune_long \
 --model vit-long \
 --epochs 200 \
 --warmup_epochs 20 \
---batch_size 128 \
+--batch_size 64 \
 --subset_ratio 1.0 \
 --pos_weight 2.8232  \
 --input_size 4200 \
