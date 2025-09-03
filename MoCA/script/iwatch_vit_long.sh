0678ae6b214d8d5355786858426bc022da8c861f
yuc0805 <yuc204@ucsd.edu>
2025-09-03 20:56:03 +0000

    sync

--- MoCA/script/iwatch_vit_long.sh
+++ MoCA/script/iwatch_vit_long.sh
@@ -18,23 +18,23 @@ torchrun --nproc_per_node=4 -m main_finetune_long \
 --use_pos_embed \
 --patch_emb 'sundial'
 
-torchrun --nproc_per_node=4 -m main_finetune_long \
---ds_name iwatch \
---data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
---remark vit-long \
---blr 1e-3 \
---weight_decay 1e-1 \
---model vit-long \
---epochs 200 \
---warmup_epochs 20 \
---batch_size 64 \
---subset_ratio 1.0 \
---pos_weight 2.8232  \
---input_size 4200 \
---patch_size 100 \
---use_data_aug 1 \
---use_pos_embed \
---patch_emb 'sundial'
+# torchrun --nproc_per_node=4 -m main_finetune_long \
+# --ds_name iwatch \
+# --data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
+# --remark vit-long \
+# --blr 1e-3 \
+# --weight_decay 1e-1 \
+# --model vit-long \
+# --epochs 200 \
+# --warmup_epochs 20 \
+# --batch_size 64 \
+# --subset_ratio 1.0 \
+# --pos_weight 2.8232  \
+# --input_size 4200 \
+# --patch_size 100 \
+# --use_data_aug 1 \
+# --use_pos_embed \
+# --patch_emb 'sundial'
 
 # chmod +x script/iwatch_vit_long.sh
 # ./script/iwatch_vit_long.sh
