6f01fe21d58efd76253e1153df7163cf10f351e3
yuc0805 <yuc204@ucsd.edu>
2025-09-04 18:39:52 -0400

    new bash

--- MoCA/script/moca_shallow_sol.sh
+++ MoCA/script/moca_shallow_sol.sh
@@ -3,7 +3,8 @@ pip install transformers
 torchrun --nproc_per_node=4 -m main_finetune_long \
 --ds_name iwatch \
 --data_path "/niddk-data-central/SOL/PASOS/train/SOL_10hz" \
---remark moca-shallow-random-init \
+--remark moca-shallow \
+--checkpoint "/niddk-data-central/leo_workspace/MoCA_result/ckpt/SOLps_5_mask_0.5_bs_12_blr_None_epoch_100/2025-08-22_03-32/checkpoint-99.pth" \
 --blr 1e-3 \
 --model shallow-moca \
 --epochs 20 \
