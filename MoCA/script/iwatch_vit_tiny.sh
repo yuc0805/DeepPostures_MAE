ad530b27a2567a891ab68440c29cd0ed0cdd4d52
yuc0805 <yuc204@ucsd.edu>
2025-09-09 15:58:04 +0000

    vit tiny

---
+++ MoCA/script/iwatch_vit_tiny.sh
@@ -0,0 +1,39 @@
+pip install transformers
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--remark vit-tiny \
+--blr 5e-4 \
+--weight_decay 5e-2 \
+--layer_decay 1.0 \
+--model vit-tiny \
+--epochs 200 \
+--warmup_epochs 20 \
+--batch_size 64 \
+--subset_ratio 1.0 \
+--pos_weight 2.7953  \
+--input_size 4200 \
+--patch_size 100 \
+--use_data_aug 1 \
+--use_pos_embed \
+--patch_emb 'sundial' 
+
+torchrun --nproc_per_node=4 -m main_finetune_long \
+--ds_name iwatch \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
+--remark vit-tiny \
+--blr 5e-4 \
+--weight_decay 5e-2 \
+--layer_decay 1.0 \
+--model vit-tiny \
+--epochs 200 \
+--warmup_epochs 20 \
+--batch_size 64 \
+--subset_ratio 1.0 \
+--pos_weight 2.8232   \
+--input_size 4200 \
+--patch_size 100 \
+--use_data_aug 1 \
+--use_pos_embed \
+--patch_emb 'sundial' 
