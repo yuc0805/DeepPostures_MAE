0d6392a596a89e97572740270c270cce2d43dc22
yuc0805 <yuc204@ucsd.edu>
2025-09-11 09:01:43 -0400

    bash

--- MoCA/script/iwatch_vit_long_wrist.sh
+++ MoCA/script/iwatch_vit_long_wrist.sh
@@ -2,17 +2,17 @@ pip install transformers
 
 torchrun --nproc_per_node=2 -m main_finetune_long \
 --ds_name iwatch \
---data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
 --remark vit-base \
---blr 5e-4 \
---weight_decay 5e-2 \
+--blr 1e-3 \
+--weight_decay 1e-3 \
 --layer_decay 1.0 \
 --model vit-base \
 --epochs 40 \
 --warmup_epochs 8 \
 --batch_size 64 \
 --subset_ratio 1.0 \
---pos_weight 2.7953  \
+--pos_weight 2.8232  \
 --input_size 4200 \
 --patch_size 100 \
 --use_data_aug 1 \
@@ -22,17 +22,17 @@ torchrun --nproc_per_node=2 -m main_finetune_long \
 
 torchrun --nproc_per_node=2 -m main_finetune_long \
 --ds_name iwatch \
---data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
 --remark vit-small \
---blr 5e-4 \
---weight_decay 5e-2 \
+--blr 1e-3 \
+--weight_decay 1e-3 \
 --layer_decay 1.0 \
 --model vit-small \
 --epochs 40 \
 --warmup_epochs 8 \
 --batch_size 64 \
 --subset_ratio 1.0 \
---pos_weight 2.7953  \
+--pos_weight 2.8232  \
 --input_size 4200 \
 --patch_size 100 \
 --use_data_aug 1 \
@@ -41,17 +41,17 @@ torchrun --nproc_per_node=2 -m main_finetune_long \
 
 torchrun --nproc_per_node=2 -m main_finetune_long \
 --ds_name iwatch \
---data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+--data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
 --remark vit-small \
---blr 5e-4 \
---weight_decay 5e-2 \
+--blr 1e-3 \
+--weight_decay 1e-3 \
 --layer_decay 1.0 \
 --model vit-tiny \
 --epochs 40 \
 --warmup_epochs 8 \
 --batch_size 64 \
 --subset_ratio 1.0 \
---pos_weight 2.7953  \
+--pos_weight 2.8232  \
 --input_size 4200 \
 --patch_size 100 \
 --use_data_aug 1 \
