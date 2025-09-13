b9f7693c9f46dd7cb7c226221084c05ff6a6d396
yuc0805 <yuc204@ucsd.edu>
2025-09-13 14:28:53 -0400

    sync

--- MoCA/script/iwatch_vit_tiny.sh
+++ MoCA/script/iwatch_vit_tiny.sh
@@ -1,23 +1,23 @@
 pip install transformers
 
-torchrun --nproc_per_node=2 -m main_finetune_long \
---ds_name iwatch \
---data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
---remark vit-base \
---blr 1e-3 \
---weight_decay 1e-3 \
---layer_decay 1.0 \
---model vit-base \
---epochs 40 \
---warmup_epochs 8 \
---batch_size 64 \
---subset_ratio 1.0 \
---pos_weight 2.7953  \
---input_size 4200 \
---patch_size 100 \
---use_data_aug 1 \
---use_pos_embed \
---patch_emb 'sundial'
+# torchrun --nproc_per_node=2 -m main_finetune_long \
+# --ds_name iwatch \
+# --data_path "/niddk-data-central/iWatch/pre_processed_long_seg/H" \
+# --remark vit-base \
+# --blr 1e-3 \
+# --weight_decay 1e-3 \
+# --layer_decay 1.0 \
+# --model vit-base \
+# --epochs 40 \
+# --warmup_epochs 8 \
+# --batch_size 64 \
+# --subset_ratio 1.0 \
+# --pos_weight 2.7953  \
+# --input_size 4200 \
+# --patch_size 100 \
+# --use_data_aug 1 \
+# --use_pos_embed \
+# --patch_emb 'sundial'
 
 
 torchrun --nproc_per_node=2 -m main_finetune_long \
@@ -41,24 +41,24 @@ torchrun --nproc_per_node=2 -m main_finetune_long \
 
 
 
-torchrun --nproc_per_node=2 -m main_finetune_long \
---ds_name iwatch \
---data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
---remark vit-base \
---blr 1e-3 \
---weight_decay 1e-3 \
---layer_decay 1.0 \
---model vit-base \
---epochs 40 \
---warmup_epochs 8 \
---batch_size 64 \
---subset_ratio 0.5 \
---pos_weight 2.8232  \
---input_size 4200 \
---patch_size 100 \
---use_data_aug 1 \
---use_pos_embed \
---patch_emb 'sundial' 
+# torchrun --nproc_per_node=2 -m main_finetune_long \
+# --ds_name iwatch \
+# --data_path "/niddk-data-central/iWatch/pre_processed_long_seg/W" \
+# --remark vit-base \
+# --blr 1e-3 \
+# --weight_decay 1e-3 \
+# --layer_decay 1.0 \
+# --model vit-base \
+# --epochs 40 \
+# --warmup_epochs 8 \
+# --batch_size 64 \
+# --subset_ratio 0.5 \
+# --pos_weight 2.8232  \
+# --input_size 4200 \
+# --patch_size 100 \
+# --use_data_aug 1 \
+# --use_pos_embed \
+# --patch_emb 'sundial' 
 
 # chmod +x script/iwatch_vit_tiny.sh
 # ./script/iwatch_vit_tiny.sh
